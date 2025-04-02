b6136ded9603d1ecbbb6d11337e6d5aa43b90715
Alexd1006 <alex3nocode@gmail.com>
2025-04-02 15:44:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import pygame
 import random
 
-from pygame.examples.resizing_new import screen
-
 pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра в Тир")
-icon =  pygame.image.load("img/Tir.png")
+icon = pygame.image.load("img/Tir.png")
 pygame.display.set_icon(icon)
-screen.blit(icon, (0, 0))
+
+splash_img = pygame.image.load("img/Tir.png")
+screen.blit(splash_img, (0, 0))
 pygame.display.update()
-
-
+pygame.time.delay(1000)
 
 target_img = pygame.image.load("img/target.png")
 target_width = 80
@@ -25,23 +24,25 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-running= True
+clock = pygame.time.Clock()
+running = True
+
 while running:
     screen.fill(color)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        if target_x<mouse_x<target_x+target_width and target_y<mouse_y<target_y+target_height:
-            target_x = random.randint(0, SCREEN_WIDTH - target_width)
-            target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
     screen.blit(target_img, (target_x, target_y))
     pygame.display.update()
-
-
+    clock.tick(60)  # Ограничение FPS до 60
 
 pygame.quit()
